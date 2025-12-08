bd7ccee6f5e1dda661db25b437c94cfc4b304daa
nbaulib <69161557+nbaulib@users.noreply.github.com>
2025-12-08 17:55:52 -0500

    add image to campus card :q
    exit
    clear
    ''
    x
    
    ;;

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -7,6 +7,8 @@ It constructs a React component to display all campuses.
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
+const defaultImg = "https://media.istockphoto.com/id/1187802081/photo/new-york-city-skyline-with-central-park.webp?b=1&s=170667a&w=0&k=20&c=0RhKXsePfAC7qyWNZGiQu2jM45269kUhf6GtldLguq8=";
+
 const AllCampusesView = (props) => {
   // If there is no campus, display a message.
   if (!props.allCampuses.length) {
@@ -20,20 +22,23 @@ const AllCampusesView = (props) => {
 
       {props.allCampuses.map((campus) => (
         <div key={campus.id} class="card">
+          <img
+            src={campus.imageUrl || defaultImg}
+            alt={campus.name}
+          />
           <Link to={`/campus/${campus.id}`}>
             <h2>{campus.name}</h2>
           </Link>
           <h4>campus id: {campus.id}</h4>
           <p>{campus.address}</p>
           <p>{campus.description}</p>
-          {/* <hr/> */}
         </div>
       ))}
-      <br/>
+      <br />
       <Link to={`/`}>
         <button>Add New Campus</button>
       </Link>
-      <br/><br/>
+      <br /><br />
     </div>
   );
 };

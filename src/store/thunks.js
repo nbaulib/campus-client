92da0ad5b12626dfdf6766bbb0ecaf2d5459acb1
nbaulib <69161557+nbaulib@users.noreply.github.com>
2025-12-11 19:55:48 -0500

    fix typo campus to campuses in addCampusThunk

--- src/store/thunks.js
+++ src/store/thunks.js
@@ -37,7 +37,7 @@ export const fetchCampusThunk = (id) => async (dispatch) => {  // The THUNK
 export const addCampusThunk = (campus) => async (dispatch) => {  // The THUNK
   try {
     // API "post" call to add "student" object's data to database
-    let res = await axios.post(`/api/campus`, campus);  
+    let res = await axios.post(`/api/campuses`, campus);  
     // Call Action Creator to return Action object (type + payload with new students data)
     // Then dispatch the Action object to Reducer to update state 
     dispatch(ac.addCampus(res.data));

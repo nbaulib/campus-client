07b317719624304e7c3089dcb902ab0a02345090
nbaulib <69161557+nbaulib@users.noreply.github.com>
2025-12-12 22:39:58 -0500

    EditStudentContainer import route

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
   NewStudentContainer,
   NewCampusContainer,
   EditCampusContainer,
-  // EditStudentContainer
+  EditStudentContainer
 } from './components/containers';
 
 // if you create separate components for adding/editing 
@@ -33,7 +33,7 @@ const App = () => {
         <Route exact path="/students" component={AllStudentsContainer} />
         <Route exact path="/newstudent" component={NewStudentContainer} />
         <Route exact path="/student/:id" component={StudentContainer} />
-        {/* <Route exact path="/editstudent/:id" component={EditStudentContainer} /> */}
+        <Route exact path="/editstudent/:id" component={EditStudentContainer} />
       </Switch>
     </div>
   );
